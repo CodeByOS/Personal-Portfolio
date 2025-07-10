921316ee3e89ded861c91cd973c69dc53fe2b966
Oussama <saidi.oussama2004@gmail.com>
2025-07-10 17:23:50 +0100

    Updates..

---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'black-100': 'theme(color-black-100)', // use the variable defined in @theme inline
+      },
+    },
+  },
+  plugins: [],
+}
