2251a0ab2ee101e78b0fd9148915f92d20add318
Oussama <saidi.oussama2004@gmail.com>
2025-07-17 19:23:15 +0100

    Update Recent Projects Section..

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -6,16 +6,16 @@ import { CircleArrowOutUpRight } from 'lucide-react'
 const RecentProjects = () => {
   return (
     <div className="py-20">
-        <h1 className="heading text-lg sm:text-xl md:text-2xl lg:text-3xl font-light">
+        <h1 className="heading">
             Explore some of my{' '}
-            <span className="text-purple-400 font-normal">latest projects</span>
+            <span className="text-purple-400 font-semibold">latest projects</span>
         </h1>
 
-    <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
+    <div className="flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-4 mt-10">
         {projects.map(({ id, title, des, img, iconLists, link }) => (
-            <div key={id} className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]">
+            <div key={id} className="sm:h-[41rem] h-[32rem] lg:min-h-[32.5rem] flex items-center justify-center sm:w-[570px] w-[80vw]">
                 <PinContainer title={link} href={link}>
-                    <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh]">
+                    <div className="relative flex items-center justify-center sm:w-[570px] w-[80vw] overflow-hidden sm:h-[40vh] h-[30vh] mb-10">
                         <div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]">
                             <img src="./bg.png" alt="bg-img" />
                         </div>
