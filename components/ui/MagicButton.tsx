5cf72dc1daa456ceaea508cd1e54378b211123ff
Oussama <saidi.oussama2004@gmail.com>
2025-07-28 18:54:06 +0100

    Updates..

--- components/ui/MagicButton.tsx
+++ components/ui/MagicButton.tsx
@@ -21,7 +21,7 @@ const MagicButton = ({
     <button
       type={type}
       disabled={disabled}
-      className="relative inline-flex h-12 w-full md:w-60 md:mt-10 overflow-hidden rounded-lg p-[1px] focus:outline-none disabled:opacity-50"
+      className="relative inline-flex h-12 w-full md:w-60 md:mt-10 overflow-hidden rounded-lg p-[1px] focus:outline-none disabled:opacity-50 cursor-pointer"
       onClick={handleClick}
     >
       <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
